a4c55efaedeb8125cab0894918f64725b2914044
Ray Walker <hello@raywalker.it>
2017-10-18 15:06:51 +1100

    bash env setup

--- tests/src/planet-4-151612/nginx-pagespeed/tests/env.bash
+++ tests/src/planet-4-151612/nginx-pagespeed/tests/env.bash
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -a
 
-compose_file=${BATS_DIRECTORY}/docker-compose.yml
+. ${BATS_TEST_DIRNAME}/../../../../env
+. ${BATS_TEST_DIRNAME}/../../../../helpers
+
+compose_file=${BATS_TEST_DIRNAME}/../docker-compose.yml
 image="${IMAGE_NAMESPACE}/${BATS_PROJECT_ID}/${BATS_IMAGE}:${IMAGE_TAG}"
 container_name="testing_${BATS_PROJECT_ID}_${BATS_IMAGE}"
