512ab6b9a4d6721c4708f00a964fdb700b63b957
Ray Walker <hello@raywalker.it>
2017-10-29 15:21:55 +1100

    Disable colours in non-interactive shell

--- src/planet-4-151612/ubuntu/bin/colours.sh
+++ src/planet-4-151612/ubuntu/bin/colours.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-CYAN='\033[0;36m'
-YELLOW='\033[1;33m'
-LIGHTCYAN='\033[1;36m'
-NC='\033[0m'
+# https://unix.stackexchange.com/questions/26676/how-to-check-if-a-shell-is-login-interactive-batch
+if [[ $- == *i* ]]
+then
+  RED='\033[0;31m'
+  GREEN='\033[0;32m'
+  CYAN='\033[0;36m'
+  YELLOW='\033[1;33m'
+  LIGHTCYAN='\033[1;36m'
+  NC='\033[0m'
+else
+  RED=
+  GREEN=
+  CYAN=
+  YELLOW=
+  LIGHTCYAN=
+  NC=
+fi
 
 _title() {
     printf "${CYAN}*** ${LIGHTCYAN}$@${NC}\n"
