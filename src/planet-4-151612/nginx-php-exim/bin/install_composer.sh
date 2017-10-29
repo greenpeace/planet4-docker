5d16f8527027848fd907283181d557dac4f08aa7
Ray Walker <hello@raywalker.it>
2017-10-29 13:49:56 +1100

    More robust composer install using wget. No need to use PHP to download when we know there are more appropriate tools installed.

--- src/planet-4-151612/nginx-php-exim/bin/install_composer.sh
+++ src/planet-4-151612/nginx-php-exim/bin/install_composer.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-# Find updated scripts at:
+# Description: Install composer from latest source
+# Author: Raymond Walker <raymond.walker@greenpeace.org>
 # https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+# Modified from original script to:
+#  - fail on any script errors
+#  - retry connection failures using wget
+#  - install hirak/prestissimo (parallel composer dependency downloader)
+
 
 retries=5
 
 EXPECTED_SIGNATURE=$(wget --retry-connrefused --waitretry=1 -t $retries -q -O - https://composer.github.io/installer.sig)
 
-loop=$retries
-until php -r "copy('http://getcomposer.org/installer', 'composer-setup.php');"
-do
-  loop=$((loop - 1))
-  if [[ loop -lt 1 ]]
-  then
-    >&2 echo "Failed to download composer after $retries attempts"
-    exit 1
-  fi
-  sleep 1
-done
-
+wget --retry-connrefused --waitretry=1 -t $retries -O composer-setup.php http://getcomposer.org/installer
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
@@ -29,11 +24,10 @@ then
     exit 1
 fi
 
-# php composer-setup.php --quiet
+# Installs composer.phar to /app/bin/composer.phar
+# Available on path, wrapped by /app/bin/composer.sh
 php composer-setup.php --install-dir=/app/bin
-RESULT=$?
 rm composer-setup.php
 
+# Install parallel downloader hirak/prestissimo (latest version)
 /app/bin/composer.phar --no-plugins --no-scripts --profile -vvv global require hirak/prestissimo
-
-exit $RESULT
