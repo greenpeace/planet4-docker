e551b489e5dc251be1355c0289ea8eebe634cab7
Ray Walker <hello@raywalker.it>
2017-11-02 16:18:59 +1100

    Add build script self-tests.

--- tests/test.sh
+++ tests/test.sh
@@ -1,36 +1,66 @@
 #!/usr/bin/env bash
 set -e
 
-switches=("$@")
+# Description:  Performs Bash Automated Shell Tests
+#               Usage:
+#               ./test.sh [directory] [Bats command line switches]
+# Author:       Raymond Walker <raymond.walker@greenpeace.org>
 
+# Find real file path of current script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 source="${BASH_SOURCE[0]}"
 while [[ -h "$source" ]]
 do # resolve $source until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$source" )" && pwd )"
+  dir="$( cd -P "$( dirname "$source" )" && pwd )"
   source="$(readlink "$source")"
-  [[ $source != /* ]] && source="$DIR/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $source != /* ]] && source="$dir/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-file_dir="$( cd -P "$( dirname "$source" )" && pwd )"
-export file_dir
+TEST_BASE_DIR="$( cd -P "$( dirname "$source" )" && pwd )"
+export TEST_BASE_DIR
 
-# shellcheck source=./env
-. ${file_dir}/env
-# shellcheck source=./helpers
-. ${file_dir}/helpers
 
-for project_dir in ${file_dir}/src/*/
-do
-  project=$(basename ${project_dir})
-  for image_dir in ${project_dir}/*/
+# Include base project helper functions
+# shellcheck source=/dev/null
+. ${TEST_BASE_DIR}/_helpers
+
+# Pass any remaining parameters to bats
+bats_switches=("$@")
+
+# If first parameter is name of a directory, assume that's the test base
+if [[ "$1" ]] && [[ -d ${TEST_BASE_DIR}/${1} ]]
+then
+  echo "Testing in: ./${1}"
+  TEST_BASE_DIR="${TEST_BASE_DIR}/${1}"
+  shift
+
+  # Include any new helper functionality in this subdirectory
+  if [[ -f ${TEST_BASE_DIR}/_helpers ]]
+  then
+    # shellcheck source=/dev/null
+    . ${TEST_BASE_DIR}/_helpers
+  fi
+
+  bats "${bats_switches[@]}"
+
+else
+  echo "Performing self tests..."
+  bats "${bats_switches[@]}" "${TEST_BASE_DIR}/self"
+
+  # Testing main project suite
+  echo "Testing in ./src"
+
+  shopt -s nullglob
+  for project_dir in ${TEST_BASE_DIR}/src/*/
   do
-    image=$(basename ${image_dir})
-    >&2 echo -e "\n >> ${project}/${image}:${IMAGE_TAG}"
-    if [[ -d "${image_dir}/tests" ]]
-    then
-      bats "${switches[@]}" ${image_dir}/tests
-    else
-      >&2 echo "WARNING: ${image_dir} contains no tests!"
-    fi
+    for image_dir in ${project_dir}/*/
+    do
+      if [[ -d ${image_dir}/tests ]]
+      then
+        bats "${bats_switches[@]}" "${image_dir}/tests"
+      else
+        >&2 echo "WARNING: ${image_dir} contains no tests!"
+      fi
 
+    done
   done
-done
+fi
