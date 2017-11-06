116a3247c8cc2622a0ef403216d27d43d0866acb
Ray Walker <hello@raywalker.it>
2017-11-07 10:44:53 +1100

    Fix colour display helpers.

--- src/planet-4-151612/ubuntu/bin/colours.sh
+++ src/planet-4-151612/ubuntu/bin/colours.sh
@@ -4,39 +4,43 @@ set -e
 # Description: Provides a bright, colourful output wrapper for scripts
 # Author:      Raymond Walker <raymond.walker@greenpeace.org>
 
-# https://unix.stackexchange.com/questions/26676/how-to-check-if-a-shell-is-login-interactive-batch
-if [[ $- == *i* ]]
-then
-  RED='\033[0;31m'
-  GREEN='\033[0;32m'
-  CYAN='\033[0;36m'
-  YELLOW='\033[1;33m'
-  LIGHTCYAN='\033[1;36m'
-  NC='\033[0m'
-else
-  RED=
-  GREEN=
-  CYAN=
-  YELLOW=
-  LIGHTCYAN=
-  NC=
+# Check if stdout is a terminal
+if test -t 1; then
+
+    # Check that it supports colours
+    ncolors=$(tput colors)
+
+    if test -n "$ncolors" && test $ncolors -ge 8; then
+        bold="$(tput bold)"
+        underline="$(tput smul)"
+        standout="$(tput smso)"
+        normal="$(tput sgr0)"
+        black="$(tput setaf 0)"
+        red="$(tput setaf 1)"
+        green="$(tput setaf 2)"
+        yellow="$(tput setaf 3)"
+        blue="$(tput setaf 4)"
+        magenta="$(tput setaf 5)"
+        cyan="$(tput setaf 6)"
+        white="$(tput setaf 7)"
+    fi
 fi
 
 _title() {
-    printf "${CYAN}*** ${LIGHTCYAN}" "$@" "${NC}\n"
+    printf "${cyan}*** ${bold}%s${normal}\n" "$@"
 }
 
 _good() {
-    printf "${GREEN}  * ${NC}" "$@" "\n"
+    printf "${green}  * ${normal}%s\n" "$@"
 }
 
 _error() {
-    2>&1 printf "  ${RED}* [ERROR]${NC} " "$@" "\n\n"
+    >&2 printf "\n${red}${standout}[ERROR]${normal} ${red}%s${normal}\n\n" "$@"
     exit 1;
 }
 
 _warning() {
-    2>&1 printf "  ${YELLOW}* [WARNING]${NC} " "$@" "\n"
+    >&2 printf "  ${yellow}* ${bold}[WARNING] ${normal}%s\n" "$@"
 }
 
 export -f _title
