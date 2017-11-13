e366282f4325d3cfe7cb4acbf7140348ae9d990f
Ray Walker <hello@raywalker.it>
2017-11-13 21:23:47 +1100

    Move env updates for wordpress to late in the hierarchy

---
+++ src/planet-4-151612/wordpress/env/90_wp_env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Generated file from /etc/my_init.d/10_initialise_environment
+# Intended to be sourced from each script which depends on consolidated vars
+
+# Environment variables will be appended here:
