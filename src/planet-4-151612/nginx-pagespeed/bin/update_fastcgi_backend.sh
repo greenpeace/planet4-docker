abb2e18e68e3bf90517e475db1049ced8d0d2fc5
Ray Walker <hello@raywalker.it>
2017-11-13 21:08:52 +1100

    Cleaning nginx-pagespeed

--- src/planet-4-151612/nginx-pagespeed/bin/update_fastcgi_backend.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# replace PHP upstream
-_good "nginx:    upstream  ${NGINX_FASTCGI_BACKEND:-$DEFAULT_NGINX_FASTCGI_BACKEND}"
-sed -i -r "s#server:.*;#server: ${NGINX_FASTCGI_BACKEND:-$DEFAULT_NGINX_FASTCGI_BACKEND};#g" /etc/nginx/sites-enabled/upstream.conf
-
-# reload configuration if running
-sv reload nginx || true
