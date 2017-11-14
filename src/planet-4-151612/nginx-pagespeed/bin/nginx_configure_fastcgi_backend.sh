7189a498a10ed824853e98577fb3d756b3969830
Ray Walker <hello@raywalker.it>
2017-11-14 11:58:14 +1100

    Round-robin load balancing of comma separated list of fastcgi_backends. eg: NGINX_FASTCGI_BACKEND=php_1:9000,php_2:9000

--- src/planet-4-151612/nginx-pagespeed/bin/nginx_configure_fastcgi_backend.sh
+++ src/planet-4-151612/nginx-pagespeed/bin/nginx_configure_fastcgi_backend.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+NGINX_FASTCGI_BACKEND=${NGINX_FASTCGI_BACKEND:-${DEFAULT_NGINX_FASTCGI_BACKEND}}
+export NGINX_FASTCGI_BACKEND
+
 dockerize -template /app/templates/etc/nginx/server.d/00_php.conf.tmpl:/etc/nginx/server.d/00_php.conf
 
 dockerize -template /app/templates/etc/nginx/sites-enabled/upstream.conf.tmpl:/etc/nginx/sites-enabled/upstream.conf
 
-[[ ${PHP_ENABLED} = 'true' ]] && _good "$(printf "%-10s " "nginx:")" "fastcgi_backend=${NGINX_FASTCGI_BACKEND:${DEFAULT_NGINX_FASTCGI_BACKEND}} "
+[[ ${PHP_ENABLED} = 'true' ]] && _good "$(printf "%-10s " "nginx:")" "fastcgi_backend=${NGINX_FASTCGI_BACKEND}"
 
 # Reload configuration if running
 if [[ $(pgrep nginx > /dev/null 2>&1) ]]
