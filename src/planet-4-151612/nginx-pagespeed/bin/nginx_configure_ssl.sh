7b2a0e2652edd771a0979ee8102b8eec03e5ab8f
Ray Walker <hello@raywalker.it>
2017-11-14 16:36:43 +1100

    Generate SSL certificates quietly.

--- src/planet-4-151612/nginx-pagespeed/bin/nginx_configure_ssl.sh
+++ src/planet-4-151612/nginx-pagespeed/bin/nginx_configure_ssl.sh
@@ -14,7 +14,7 @@ else
 		# Generate Diffie Hellman parameter file, as the default shipped with Ubuntu is 1024bits and considered insecure
 		# https://wiki.mozilla.org/Security/Server_Side_TLS#DHE_handshake_and_dhparam
 		_good "$(printf "%-10s " "nginx:")" "Generating /etc/nginx/ssl/${APP_HOSTNAME}.dhparam.pem ..."
-		openssl dhparam -out "/etc/nginx/ssl/${APP_HOSTNAME}.dhparam.pem" 2048 > /dev/null 2>&1 &
+		openssl dhparam -out "/etc/nginx/ssl/${APP_HOSTNAME}.dhparam.pem" 2048 >/dev/null 2>&1 &
 	fi
 
 	if [[ ! -f "/etc/nginx/ssl/${APP_HOSTNAME}.key" ]]
@@ -22,7 +22,7 @@ else
 		# Generate a self signed certificate if one does not exist
 		# Expires in 365 days
 		_good "$(printf "%-10s " "nginx:")" "Generating default /etc/nginx/ssl/${APP_HOSTNAME}.key /etc/nginx/ssl/${APP_HOSTNAME}.crt"
-		openssl req -x509 -newkey rsa:2048 -keyout "/etc/nginx/ssl/${APP_HOSTNAME}.key" -out "/etc/nginx/ssl/${APP_HOSTNAME}.crt" -nodes -days 365 -subj "/C=NL/ST=State/L=Location/O=/OU=/CN=localdomain" &
+		openssl req -x509 -newkey rsa:2048 -keyout "/etc/nginx/ssl/${APP_HOSTNAME}.key" -out "/etc/nginx/ssl/${APP_HOSTNAME}.crt" -nodes -days 365 -subj "/C=NL/ST=State/L=Location/O=/OU=/CN=localdomain" >/dev/null 2>&1 &
 	fi
 
 fi
