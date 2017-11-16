59d7ae60bb502f7f57f0fa0cf98f63bb6f26871a
Ray Walker <hello@raywalker.it>
2017-11-17 10:38:18 +1100

    Ensure that composer directories are owned by APP_USER:APP_GROUP

--- src/planet-4-151612/php-fpm/bin/composer.sh
+++ src/planet-4-151612/php-fpm/bin/composer.sh
@@ -3,5 +3,9 @@
 # Description: Wraps Composer to provide correct user permissions
 # Author:      Raymond Walker <raymond.walker@greenpeace.org>
 
+if [[ $(stat -c "%U" "${COMPOSER_HOME}") != "${APP_USER:-$DEFAULT_APP_USER}" ]] || [[ "$(stat -c "%U" /app/.composer)" != "${APP_GROUP:-$DEFAULT_APP_GROUP}" ]]
+then
+  chown -R ${APP_USER:-$DEFAULT_APP_USER}:${APP_GROUP:-$DEFAULT_APP_GROUP} /app
+fi
 
 exec setuser ${APP_USER:-$DEFAULT_APP_USER} php /app/bin/composer.phar "$@"
