562308df05edf16341a85521c277a083155f3377
Ray Walker <hello@raywalker.it>
2017-11-17 10:35:55 +1100

    No need to delete the faulty composer-setup.php. And it may be helpful in debugging container failures.

--- src/planet-4-151612/php-fpm/bin/php_install_composer.sh
+++ src/planet-4-151612/php-fpm/bin/php_install_composer.sh
@@ -19,7 +19,6 @@ ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
 then
     >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
     exit 1
 fi
 
