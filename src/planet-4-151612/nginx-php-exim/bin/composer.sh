29f92dfa9eabc8ab1ec69793b2780ef62bb26cf5
Ray Walker <hello@raywalker.it>
2017-11-19 20:55:53 +1100

    Clean unused nginx-php-exim image.

--- src/planet-4-151612/nginx-php-exim/bin/composer.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Description: Wraps Composer to provide correct user permissions
-# Author:      Raymond Walker <raymond.walker@greenpeace.org>
-
-
-exec setuser ${APP_USER:-$DEFAULT_APP_USER} php /app/bin/composer.phar "$@"
