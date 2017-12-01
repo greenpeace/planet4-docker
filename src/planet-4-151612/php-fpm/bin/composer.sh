b33139816908c65bf588a5058ac67a2bfe11ef79
Ray Walker <geek@raywalker.it>
2017-12-01 15:30:39 +1100

    chown composer directories before use.

--- src/planet-4-151612/php-fpm/bin/composer.sh
+++ src/planet-4-151612/php-fpm/bin/composer.sh
@@ -8,6 +8,11 @@ uid=$(id -u)
 
 if [[ $uid = "0" ]]
 then
+  if [[ $(ls -ldn . | awk '{print $3}') != "${APP_USER:-$DEFAULT_APP_USER}" ]]
+  then
+    chown -R ${APP_USER:-$DEFAULT_APP_USER} /app/.composer
+    chown -R ${APP_USER:-$DEFAULT_APP_USER} /app/source
+  fi
   exec setuser ${APP_USER:-$DEFAULT_APP_USER} php /app/bin/composer.phar "$@"
 elif [[ $uid = "${APP_UID:-${DEFAULT_APP_UID}}" ]]
 then
