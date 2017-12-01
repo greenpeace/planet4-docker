b70967569b97978b4671335eda1afe7622aa055f
Ray Walker <geek@raywalker.it>
2017-12-01 12:48:42 +1100

    Don't bother pulling realpath code from gist, too much drama.

--- bin/build.sh
+++ bin/build.sh
@@ -2,7 +2,22 @@
 # shellcheck disable=1090,2016
 set -eou pipefail
 
-. inc/main
+# ----------------------------------------------------------------------------
+
+# Find real file path of current script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+
+source="${BASH_SOURCE[0]}"
+while [[ -h "$source" ]]
+do # resolve $source until the file is no longer a symlink
+  dir="$( cd -P "$( dirname "$source" )" && pwd )"
+  source="$(readlink "$source")"
+  [[ $source != /* ]] && source="$dir/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+GIT_ROOT_DIR="$( cd -P "$( dirname "$source" )/.." && pwd )"
+export GIT_ROOT_DIR
+
+. "${GIT_ROOT_DIR}/bin/inc/main"
 
 # UTILITY
 
