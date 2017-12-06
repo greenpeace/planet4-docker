069056ed31f80862a119ada5404c04cf2ae54b22
Ray Walker <geek@raywalker.it>
2017-12-06 13:35:18 +1100

    Clean testing containers, volumes.

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
 # ----------------------------------------------------------------------------
 
@@ -17,3 +17,30 @@ GIT_ROOT_DIR="$( cd -P "$( dirname "$source" )/.." && pwd )"
 
 find "${GIT_ROOT_DIR}/src" -name "Dockerfile" -exec rm -r "{}" \;
 find "${GIT_ROOT_DIR}/tests/src" -name "Dockerfile" -exec rm -r "{}" \;
+
+test_containers=(
+  "p4sampleapplication_openresty" \
+  "p4sampleapplication_php-fpm" \
+  "p4sampleapplication_db" \
+)
+
+for container in "${test_containers[@]}"
+do
+  # FIXME get actual container numbers properly
+  # stop running container
+  docker stop ${container}_1 >/dev/null 2>&1 &
+  docker stop ${container}_2 >/dev/null 2>&1 &
+  docker stop ${container}_3 >/dev/null 2>&1 &
+  docker stop ${container}_4 >/dev/null 2>&1 &
+  wait
+  docker rm ${container}_1 >/dev/null 2>&1 &
+  docker rm ${container}_2 >/dev/null 2>&1 &
+  docker rm ${container}_3 >/dev/null 2>&1 &
+  docker rm ${container}_4 >/dev/null 2>&1 &
+  wait
+  docker rmi -f ${container}
+done
+docker network rm p4sampleapplication_default || true
+docker volume rm -f p4sampleapplication_data || true
+
+echo -e "\nClean"
