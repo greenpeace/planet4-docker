373a196d90e3bbbe1a95b04507006bc66f78bf06
Ray Walker <geek@raywalker.it>
2017-12-06 13:35:38 +1100

    Break ./go.sh tasks into separate lines.

--- go.sh
+++ go.sh
@@ -3,4 +3,12 @@
 # Yes, this expects planet4-base to be a sibling directory. Make it so!
 # https://github.com/greenpeace/planet4-base
 
-time ( make clean; make build ${1:+BUILD_FLAGS=${1}} ${2:+BUILD_LIST=${2}}; pushd "../planet4-base"; make build FROM_TAG=develop BUILD_FLAGS=-rp; beep.sh; popd )
+time ( make clean; \
+  make build ${1:+BUILD_FLAGS=${1}} ${2:+BUILD_LIST=${2}}; \
+  pushd "../planet4-base"; \
+  make build FROM_TAG=develop BUILD_FLAGS=-rp; \
+  beep.sh; \
+  popd; \
+  make test;
+  beep.sh -r 3 \
+)
