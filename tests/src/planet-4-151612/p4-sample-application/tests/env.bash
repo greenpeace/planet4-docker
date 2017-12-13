525a62afae5411839321b7b6abb5f8987e3d854d
Ray Walker <geek@raywalker.it>
2017-12-13 14:36:58 +1100

    Stray unquoted path expansion.

--- tests/src/planet-4-151612/p4-sample-application/tests/env.bash
+++ tests/src/planet-4-151612/p4-sample-application/tests/env.bash
@@ -15,7 +15,7 @@ fi
 . "${BATS_TEST_DIRNAME}/../../../../_helpers"
 
 # bats test parameters
-compose_file=${BATS_TEST_DIRNAME}/../docker-compose.yml
+compose_file="${BATS_TEST_DIRNAME}/../docker-compose.yml"
 container_name="testing_${BATS_PROJECT_ID}_${BATS_IMAGE}"
 image="${IMAGE_NAMESPACE}/${BATS_PROJECT_ID}/${BATS_IMAGE}:${P4_SAMPLE_APPLICATION_FROM_TAG:-${IMAGE_TAG}}"
 
