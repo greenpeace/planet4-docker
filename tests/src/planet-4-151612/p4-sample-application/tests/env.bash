c69959d7febe1b2f75604c0c07c085e15d1937bc
Ray Walker <geek@raywalker.it>
2017-12-14 22:46:56 +1100

    Remove unused variable.

--- tests/src/planet-4-151612/p4-sample-application/tests/env.bash
+++ tests/src/planet-4-151612/p4-sample-application/tests/env.bash
@@ -17,7 +17,8 @@ fi
 # bats test parameters
 compose_file="${BATS_TEST_DIRNAME}/../docker-compose.yml"
 container_name="testing_${BATS_PROJECT_ID}_${BATS_IMAGE}"
-image="${IMAGE_NAMESPACE}/${BATS_PROJECT_ID}/${BATS_IMAGE}:${P4_SAMPLE_APPLICATION_FROM_TAG:-${IMAGE_TAG}}"
+image="${IMAGE_NAMESPACE}/${BATS_PROJECT_ID}/${BATS_IMAGE}:${IMAGE_TAG}}"
+
 
 export compose_file
 export container_name
