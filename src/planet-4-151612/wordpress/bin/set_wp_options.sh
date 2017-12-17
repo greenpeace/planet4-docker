af8a72f8366b3671ec375ca196d2bf4034677841
Ray Walker <geek@raywalker.it>
2017-12-17 22:00:36 +1100

    Formatting.

--- src/planet-4-151612/wordpress/bin/set_wp_options.sh
+++ src/planet-4-151612/wordpress/bin/set_wp_options.sh
@@ -14,10 +14,8 @@ _good "blogname         ${WP_TITLE}"
 _good "blogdescription  ${WP_TITLE}"
 _good "rewrite          ${WP_REWRITE_STRUCTURE}"
 
-wp option set siteurl "${WP_SITE_PROTOCOL}://${WP_SITE_URL:-$APP_HOSTNAME}"
-wp option set home "${WP_SITE_PROTOCOL}://${WP_SITE_HOME:-$APP_HOSTNAME}"
-wp option set blogname "${WP_TITLE}"
+wp option set siteurl         "${WP_SITE_PROTOCOL}://${WP_SITE_URL:-$APP_HOSTNAME}"
+wp option set home            "${WP_SITE_PROTOCOL}://${WP_SITE_HOME:-$APP_HOSTNAME}"
+wp option set blogname        "${WP_TITLE}"
 wp option set blogdescription "${WP_TITLE}"
-wp rewrite structure "${WP_REWRITE_STRUCTURE}"
-
-# rm $optionlock
+wp rewrite structure          "${WP_REWRITE_STRUCTURE}"
