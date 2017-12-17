365293651038abbe386bd711a2d42f9e8adb3271
Ray Walker <geek@raywalker.it>
2017-12-17 21:58:23 +1100

    Remove wp-config.php.tmpl get_env(), use Go template from script

--- src/planet-4-151612/wordpress/etc/rc.docker.d/10_wordpress.sh
+++ src/planet-4-151612/wordpress/etc/rc.docker.d/10_wordpress.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# FIXME Re-apply wordpress configuration options at container start to override
-# wp-config.php values
-
 [[ ${WP_SET_OPTIONS_ON_BOOT} == "true" ]] && /app/bin/set_wp_options.sh
 
-dockerize -template /app/wp-config.php.tmpl:/app/source/public/wp-config.php
+/app/bin/generate_wp_config.sh
