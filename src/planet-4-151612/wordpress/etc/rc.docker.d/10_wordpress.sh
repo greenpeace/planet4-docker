4aee675e0dcc304cac045e0d8e6c16d05a30d0a9
Ray Walker <geek@raywalker.it>
2017-12-19 17:07:34 +1100

    No, there probably isn't.

--- src/planet-4-151612/wordpress/etc/rc.docker.d/10_wordpress.sh
+++ src/planet-4-151612/wordpress/etc/rc.docker.d/10_wordpress.sh
@@ -4,7 +4,6 @@ set -e
 [[ ${WP_SET_OPTIONS_ON_BOOT} = "true" ]] && /app/bin/set_wp_options.sh
 
 # Install WP-Redis object cache file if exist
-# FIXME there must be a better way
 if [[ ${WP_REDIS_ENABLED} = "true" ]]
 then
   [[ -f /app/source/public/wp-content/plugins/wp-redis/object-cache.php ]] && wp redis enable
