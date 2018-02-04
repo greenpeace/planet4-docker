47d882c15b870dfdd7ff09ed969642c7abce146a
Ray Walker <geek@raywalker.it>
2018-02-05 09:06:40 +1100

    Docs

--- src/planet-4-151612/wordpress/etc/rc.docker.d/10_wordpress.sh
+++ src/planet-4-151612/wordpress/etc/rc.docker.d/10_wordpress.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+# Resets database options to environment variable, such as:
+# siteurl, home, blogname etc
 [[ ${WP_SET_OPTIONS_ON_BOOT} = "true" ]] && /app/bin/set_wp_options.sh
 
-# Install WP-Redis object cache file if exist
 if [[ ${WP_REDIS_ENABLED} = "true" ]]
 then
+  # Install WP-Redis object cache file if exist
   [[ -f /app/source/public/wp-content/plugins/wp-redis/object-cache.php ]] && wp redis enable
 else
   [[ -f /app/source/public/wp-content/object-cache.php ]] && rm -f /app/source/public/wp-content/object-cache.php
