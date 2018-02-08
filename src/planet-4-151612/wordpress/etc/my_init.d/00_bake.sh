7ef3a32603316490212c87ebf7b0760eddefecf1
Ray Walker <hello@raywalker.it>
2018-02-08 13:31:20 +1100

    Add baking script to embed source files in container.

---
+++ src/planet-4-151612/wordpress/etc/my_init.d/00_bake.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -ex
+
+[[ "$WP_BAKE" = "true" ]] || exit 0
+
+rm -fr /app/source/public
+
+composer --profile -vv copy:wordpress
+
+composer --profile -vv reset:themes
+composer --profile -vv reset:plugins
+
+composer --profile -vv copy:themes
+composer --profile -vv copy:assets
+composer --profile -vv copy:plugins
+
+composer --profile -vv core:style
+
+echo "Files baked, running away!"
+
+# Don't panic!
+exit 1
