d1d584a2651cce702679aa4faaae85ad75a15dcf
Ray Walker <hello@raywalker.it>
2018-03-18 15:26:03 +1100

    Fix image name.

--- tests/src/planet-4-151612/p4-sample-application/tests/env.bash
+++ tests/src/planet-4-151612/p4-sample-application/tests/env.bash
@@ -14,7 +14,7 @@ fi
 # bats test parameters
 compose_file="${BATS_TEST_DIRNAME}/../docker-compose.yml"
 container_name="testing_${BATS_PROJECT_ID}_${BATS_IMAGE}"
-image="${IMAGE_NAMESPACE}/${BATS_PROJECT_ID}/${BATS_IMAGE}:${IMAGE_TAG}}"
+image="${IMAGE_NAMESPACE}/${BATS_PROJECT_ID}/${BATS_IMAGE}:${IMAGE_TAG}"
 
 
 export compose_file
