d11c341a02686da29d9aecc077c33ff8a73566ae
Ray Walker <hello@raywalker.it>
2018-03-18 15:24:26 +1100

    Extract composer executable string to variable.

--- src/planet-4-151612/wordpress/etc/my_init.d/00_bake.sh
+++ src/planet-4-151612/wordpress/etc/my_init.d/00_bake.sh
@@ -4,21 +4,25 @@ set -ex
 [[ "$WP_BAKE" = "true" ]] || exit 0
 
 rm -fr /app/source/public
+composer_exec="composer --profile -vv"
+$composer_exec download:wordpress
 
-composer --profile -vv download:wordpress
+$composer_exec reset:themes
+$composer_exec reset:plugins
 
-composer --profile -vv reset:themes
-composer --profile -vv reset:plugins
+$composer_exec copy:health-check
 
-composer --profile -vv copy:health-check
+$composer_exec copy:themes
+$composer_exec copy:assets
+$composer_exec copy:plugins
 
-composer --profile -vv copy:themes
-composer --profile -vv copy:assets
-composer --profile -vv copy:plugins
+$composer_exec core:style
 
-composer --profile -vv core:style
+$composer_exec core:js
 
-echo "Files baked, running away!"
+$composer_exec site:custom
+
+echo "Files baked, returning status 1 to exit container"
 
 # Don't panic!
 exit 1
