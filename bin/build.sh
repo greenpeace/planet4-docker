4058fc6747527356173641cee0340008ffb7802c
Ray Walker <hello@raywalker.it>
2018-04-15 11:04:10 +1000

    Add MAINTAINER_NAME and MAINTAINER_EMAIL

--- bin/build.sh
+++ bin/build.sh
@@ -169,6 +169,8 @@ then
       '${CONTAINER_TIMEZONE}' \
       '${DOCKERIZE_VERSION}' \
       '${GOOGLE_PROJECT_ID}' \
+      '${MAINTAINER_NAME}' \
+      '${MAINTAINER_EMAIL}' \
       '${NGX_PAGESPEED_RELEASE}' \
       '${NGX_PAGESPEED_VERSION}' \
       '${OPENRESTY_SOURCE}' \
