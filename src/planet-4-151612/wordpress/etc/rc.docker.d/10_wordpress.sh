827bb34513d0a9c39cbc87c6abaaf6bd356c181e
Ray Walker <hello@raywalker.it>
2018-04-20 16:36:47 +1000

    Startup core checks. Install WP if not yet done. Fixes docker-compose.stateless.yaml errors.

--- src/planet-4-151612/wordpress/etc/rc.docker.d/10_wordpress.sh
+++ src/planet-4-151612/wordpress/etc/rc.docker.d/10_wordpress.sh
@@ -1,13 +1,44 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
-# Executed after my_init.d
-
-chown -R "$APP_USER:$APP_GROUP" "${PUBLIC_PATH}"
+# Executed after my_init.d and environment is established
 
 # Generates keys and salts for wp-config.php
 /app/bin/generate_wp_keys.sh
+
+# Write the wp-config.php file from template
 /app/bin/generate_wp_config.sh
+
+# Wait up to two minutes for the database to become ready
+timeout=2
+i=0
+until dockerize -wait "tcp://${WP_DB_HOST}:${WP_DB_PORT}" -timeout 60s mysql -h "${WP_DB_HOST}" -u "${WP_DB_USER}" --password="${WP_DB_PASS}" -e "use ${WP_DB_NAME}"
+do
+  let i=i+1
+  if [[ $i -gt $timeout ]]
+  then
+    _error "Timeout waiting for database to become ready"
+    exit 1
+  fi
+done
+set +e
+wp core is-installed
+is_installed=$?
+set -e
+if [[ $is_installed -ne 0 ]]
+then
+  _good "Installing Wordpress..."
+  wp core install --url="${WP_HOSTNAME}" --title="$WP_TITLE" --admin_user="${WP_ADMIN_USER:-admin}" --admin_email="${WP_ADMIN_EMAIL:-$MAINTAINER_EMAIL}"
+
+  _good "Activating plugins..."
+  wp plugin activate --all
+
+  # FIXME Determine which theme to activate
+  # FIXME Why does the composer theme install script fail?
+  _good "Activating theme: ${WP_THEME}"
+  wp theme activate "${WP_THEME}"
+fi
+_good "Wordpress v$(wp core version) installed"
 
 # Resets database options to environment variable, such as:
 # siteurl, home, blogname etc
