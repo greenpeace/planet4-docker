c96efe895a64d714e8c99eda2eb3e8d9c09ebd22
Ray Walker <hello@raywalker.it>
2018-04-27 14:01:35 +1000

    Stateless bucket creation fixes

--- src/planet-4-151612/p4-builder/make_bucket.sh
+++ src/planet-4-151612/p4-builder/make_bucket.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -exu
 
+# Exit if bucket exists, doing nothing
+gsutil ls gs://${WP_STATELESS_BUCKET} && exit 0
+
 gsutil mb gs://${WP_STATELESS_BUCKET}
 
 gsutil iam ch allUsers:objectViewer gs://${WP_STATELESS_BUCKET}
