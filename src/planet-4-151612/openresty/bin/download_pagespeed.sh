5cdd56296754ce230b4392f378cf27c3f09ce23f
Devonte <devo.tox.89@gmail.com>
2018-05-02 11:02:28 +0100

    Update to new Pageseed Location

--- src/planet-4-151612/openresty/bin/download_pagespeed.sh
+++ src/planet-4-151612/openresty/bin/download_pagespeed.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Downloading ngx_pagespeed ${NGX_PAGESPEED_VERSION} from https://github.com/pagespeed/ngx_pagespeed/archive/${NGX_PAGESPEED_VERSION}-${NGX_PAGESPEED_RELEASE}.tar.gz..."
+echo "Downloading incubator-pagespeed-ngx ${NGX_PAGESPEED_VERSION} from https://github.com/apache/incubator-pagespeed-ngx/archive/${NGX_PAGESPEED_VERSION}-${NGX_PAGESPEED_RELEASE}.tar.gz..."
 
-wget --retry-connrefused --waitretry=1 -t 5 -O - https://github.com/pagespeed/ngx_pagespeed/archive/${NGX_PAGESPEED_VERSION}-${NGX_PAGESPEED_RELEASE}.tar.gz --progress=bar --tries=3 | tar zxf - -C /tmp
+wget -O - https://github.com/apache/incubator-pagespeed-ngx/archive/${NGX_PAGESPEED_VERSION}-${NGX_PAGESPEED_RELEASE}.tar.gz --progress=bar --tries=3 \
+	| tar zxf - -C /tmp
 
-PSOL_URL=$(cat "/tmp/ngx_pagespeed-${NGX_PAGESPEED_VERSION}-${NGX_PAGESPEED_RELEASE}/PSOL_BINARY_URL")
+PSOL_URL=$(cat "/tmp/incubator-pagespeed-ngx-${NGX_PAGESPEED_VERSION}-${NGX_PAGESPEED_RELEASE}/PSOL_BINARY_URL")
 
 # The size names must match install/build_psol.sh in mod_pagespeed
 if [ "$(uname -m)" = x86_64 ]; then
@@ -13,6 +14,7 @@ else
   PSOL_BIT_SIZE_NAME=ia32
 fi
 
-echo "Downloading ngx_pagespeed PSOL ${NGX_PAGESPEED_VERSION} from ${PSOL_URL/\$BIT_SIZE_NAME/$PSOL_BIT_SIZE_NAME}..."
+echo "Downloading incubator-pagespeed-ngx PSOL ${NGX_PAGESPEED_VERSION} from ${PSOL_URL/\$BIT_SIZE_NAME/$PSOL_BIT_SIZE_NAME}..."
 
-wget --retry-connrefused --waitretry=1 -t 5 -O - ${PSOL_URL/\$BIT_SIZE_NAME/$PSOL_BIT_SIZE_NAME} | tar zxf - -C /tmp/ngx_pagespeed-${NGX_PAGESPEED_VERSION}-${NGX_PAGESPEED_RELEASE}
+wget -O - ${PSOL_URL/\$BIT_SIZE_NAME/$PSOL_BIT_SIZE_NAME} --progress=bar --tries=3 \
+	| tar zxf - -C /tmp/incubator-pagespeed-ngx-${NGX_PAGESPEED_VERSION}-${NGX_PAGESPEED_RELEASE}/
