47adb6a4d4e9b0a49d80cbeb018c7a18b6385895
Ray Walker <raymond.walker@greenpeace.org>
2018-05-12 11:26:09 +1000

    More verbose status text in notifications.

--- src/planet-4-151612/p4-builder/helm_confirm.sh
+++ src/planet-4-151612/p4-builder/helm_confirm.sh
@@ -3,21 +3,24 @@ set -eu
 
 release_status=$(helm status "${HELM_RELEASE}" -o json | jq '.info.status.code')
 
+status_text=$(helm status "${HELM_RELEASE}")
+
 if [[ ${release_status} = "1" ]]
 then
   echo "Helm release ${HELM_RELEASE} successful"
   ./flush_redis.sh
-  TYPE="Helm Deployment" EXTRA_TEXT="Containers:
-  - ${PHP_IMAGE}:${BUILD_TAG}
-  - ${OPENRESTY_IMAGE}:${BUILD_TAG}
-\`\`\`$(helm status "${HELM_RELEASE}")\`\`\`" "${HOME}/scripts/notify-job-success.sh"
+  TYPE="Helm Deployment" EXTRA_TEXT="\`\`\`
+History:
+$(helm history --max=5)
+Status:
+${status_text}\`\`\`" "${HOME}/scripts/notify-job-success.sh"
   exit 0
 fi
 
 echo "ERROR: Helm release ${HELM_RELEASE} failed to deploy"
-TYPE="Helm Deployment" EXTRA_TEXT="
-Containers:
-- ${PHP_IMAGE}:${BUILD_TAG}
-- ${OPENRESTY_IMAGE}:${BUILD_TAG}
-\`\`\`$(helm status "${HELM_RELEASE}")\`\`\`" "${HOME}/scripts/notify-job-failure.sh"
+TYPE="Helm Deployment" EXTRA_TEXT="\`\`\`
+History:
+$(helm history --max=5)
+Status:
+${status_text}\`\`\`" "${HOME}/scripts/notify-job-failure.sh"
 exit 1
