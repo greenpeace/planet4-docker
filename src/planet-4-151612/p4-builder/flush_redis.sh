ed86c34e5e0f424d9ac333391a2745252e4b3ed8
Raymond Walker <raymond.walker@greenpeace.org>
2018-05-12 16:02:18 +1000

    Words.

--- src/planet-4-151612/p4-builder/flush_redis.sh
+++ src/planet-4-151612/p4-builder/flush_redis.sh
@@ -3,5 +3,5 @@
 set -eu
 
 redis=$(kubectl get pods --namespace "${HELM_NAMESPACE}" -l "app=${HELM_RELEASE}-redis" -o jsonpath="{.items[0].metadata.name}")
-echo "Flushing redis pod ${redis}..."
+echo "Flushing redis pod ${redis} in ${HELM_NAMESPACE}..."
 kubectl --namespace "${HELM_NAMESPACE}" exec "$redis" redis-cli flushdb
