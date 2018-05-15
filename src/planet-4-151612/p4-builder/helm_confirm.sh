f9e5b343e69ab6d8592c894a8435f124ebfd71fa
Raymond Walker <raymond.walker@greenpeace.org>
2018-05-15 11:54:39 +1000

    Spacing

--- src/planet-4-151612/p4-builder/helm_confirm.sh
+++ src/planet-4-151612/p4-builder/helm_confirm.sh
@@ -9,18 +9,18 @@ if [[ ${release_status} = "1" ]]
 then
   echo "Helm release ${HELM_RELEASE} successful"
   ./flush_redis.sh
-  TYPE="Helm Deployment" EXTRA_TEXT="\`\`\`
-History:
+  TYPE="Helm Deployment" EXTRA_TEXT="\`\`\`History:
 $(helm history "${HELM_RELEASE}" --max=5)
+
 Status:
 ${status_text}\`\`\`" "${HOME}/scripts/notify-job-success.sh"
   exit 0
 fi
 
 echo "ERROR: Helm release ${HELM_RELEASE} failed to deploy"
-TYPE="Helm Deployment" EXTRA_TEXT="\`\`\`
-History:
+TYPE="Helm Deployment" EXTRA_TEXT="\`\`\`History:
 $(helm history "${HELM_RELEASE}" --max=5)
+
 Status:
 ${status_text}\`\`\`" "${HOME}/scripts/notify-job-failure.sh"
 exit 1
