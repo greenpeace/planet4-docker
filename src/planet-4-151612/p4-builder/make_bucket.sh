e06e275e53e206b6e14ae2c3337e2afdcd4f3d38
Raymond Walker <raymond.walker@greenpeace.org>
2018-05-22 14:38:25 +1000

    Set longer cache-control max-age on storage bucket content.

--- src/planet-4-151612/p4-builder/make_bucket.sh
+++ src/planet-4-151612/p4-builder/make_bucket.sh
@@ -2,8 +2,10 @@
 set -exu
 
 # Exit if bucket exists, doing nothing
-gsutil ls gs://${WP_STATELESS_BUCKET} || gsutil mb gs://${WP_STATELESS_BUCKET}
+gsutil ls "gs://${WP_STATELESS_BUCKET}" || gsutil mb "gs://${WP_STATELESS_BUCKET}"
 
-gsutil iam ch allUsers:objectViewer gs://${WP_STATELESS_BUCKET}
+gsutil iam ch allUsers:objectViewer "gs://${WP_STATELESS_BUCKET}"
 
-gsutil iam ch serviceAccount:${WP_STATELESS_OWNER}:admin gs://${WP_STATELESS_BUCKET}
+gsutil iam ch "serviceAccount:${WP_STATELESS_OWNER}:admin" "gs://${WP_STATELESS_BUCKET}"
+
+gsutil -m setmeta -r -h "Cache-Control:public, max-age=2678400" "gs://${WP_STATELESS_BUCKET}"
