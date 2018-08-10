a1e2d115ee2dbc19b178c78a096b8129878b9cfc
Raymond Walker <raymond.walker@greenpeace.org>
2018-08-10 14:59:11 +1000

    Skip setting several options, only keep rewrite option.

--- src/planet-4-151612/wordpress/bin/set_wp_options.sh
+++ src/planet-4-151612/wordpress/bin/set_wp_options.sh
@@ -12,14 +12,14 @@ touch "$optionlock"
 
 # Wordpress configuration startup
 # FIXME Set blogdescription to something useful
-_good "siteurl          ${WP_SITE_PROTOCOL}://${WP_SITE_URL:-$APP_HOSTNAME}"
-_good "home             ${WP_SITE_PROTOCOL}://${WP_SITE_HOME:-${WP_SITE_URL:-WP_SITE_URL}}"
-_good "blogname         ${WP_TITLE}"
-_good "blogdescription  ${WP_DESCRIPTION}"
+# _good "siteurl          ${WP_SITE_PROTOCOL}://${WP_SITE_URL:-$APP_HOSTNAME}"
+# _good "home             ${WP_SITE_PROTOCOL}://${WP_SITE_HOME:-${WP_SITE_URL:-WP_SITE_URL}}"
+# _good "blogname         ${WP_TITLE}"
+# _good "blogdescription  ${WP_DESCRIPTION}"
 _good "rewrite          ${WP_REWRITE_STRUCTURE}"
 
-wp option set siteurl         "${WP_SITE_PROTOCOL}://${WP_SITE_URL:-$APP_HOSTNAME}"
-wp option set home            "${WP_SITE_PROTOCOL}://${WP_SITE_HOME:-${WP_SITE_URL:-WP_SITE_URL}}"
-wp option set blogname        "${WP_TITLE}"
-wp option set blogdescription "${WP_TITLE}"
+# wp option set siteurl         "${WP_SITE_PROTOCOL}://${WP_SITE_URL:-$APP_HOSTNAME}"
+# wp option set home            "${WP_SITE_PROTOCOL}://${WP_SITE_HOME:-${WP_SITE_URL:-WP_SITE_URL}}"
+# wp option set blogname        "${WP_TITLE}"
+# wp option set blogdescription "${WP_TITLE}"
 wp rewrite structure          "${WP_REWRITE_STRUCTURE}"
