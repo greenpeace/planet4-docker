710230064796fd38aa049f23b2d332d87c1cb3c6
Raymond Walker <raymond.walker@greenpeace.org>
2019-05-03 22:28:47 +1000

    Fix path to health check template.

--- src/planet-4-151612/openresty/bin/nginx_configure_health_checks.sh
+++ src/planet-4-151612/openresty/bin/nginx_configure_health_checks.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 # Configure health checks
 
-f=/etc/nginx/conf.d/40_health-check.conf
+f=/etc/nginx/server.d/40_health-check.conf
 
 dockerize -template "/app/templates$f.tmpl:$f"
