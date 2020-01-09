83de6498195b9b4c0030cd5dd96f5d8a6d82911d
Jen <jhasenau@greenpeace.org>
2020-01-09 12:42:21 +1100

    Configure variables for Maxmind account and license keys

--- src/planet-4-151612/openresty/bin/nginx_configure_geoip2.sh
+++ src/planet-4-151612/openresty/bin/nginx_configure_geoip2.sh
@@ -1,24 +1,45 @@
 #!/usr/bin/env bash
 set -euo pipefail
+#Check License and Account key variables
 
-_good "$(printf "%-10s " "openresty:")" "GEOIP2 ${GEOIP2_ENABLED}"
+if [[ ${GEOIP2_ENABLED} = 'true' ]]
+then
+  if [[ -z "${MAXMIND_ACCOUNTID}" ]]
+  then
+    GEOIP2_ENABLED="false"
+    export GEOIP2_ENABLED
+    _warning "$(printf "%-10s " "php:")" "MAXMIND_ACCOUNTID is blank, account id is required"
+    _warning "$(printf "%-10s " "php:")" "disabling GeoIP"
+  elif [[ -z "${MAXMIND_LICENSE}" ]]
+  then
+    GEOIP2_ENABLED="false"
+    export GEOIP2_ENABLED
+    _warning "$(printf "%-10s " "php:")" "MAXMIND_LICENSE is blank, license is required"
+    _warning "$(printf "%-10s " "php:")" "disabling GeoIP"
+  else
+    _good "$(printf "%-10s " "php:")" "$(printf "%-22s" "maxmind.accountid:")" "${MAXMIND_ACCOUNTID//[[:alnum:]]/*}"
+    _good "$(printf "%-10s " "php:")" "$(printf "%-22s" "maxmind.license:")" "${MAXMIND_LICENSE//[[:alnum:]]/*}"
+    _good "$(printf "%-10s " "openresty:")" "GEOIP2 ${GEOIP2_ENABLED}"
+ fi
+fi
 
 [[ ${GEOIP2_ENABLED} = "true" ]] || {
   exit 0
 }
 
-# Update GeoIP data
-/usr/bin/geoipupdate -v &
-
 files=(
   /etc/nginx/conf.d/90_geoip.conf
   /etc/nginx/server.d/90_geoip.conf
+  /etc/GeoIP.conf
 )
 
 for f in "${files[@]}"
 do
   dockerize -template "/app/templates$f.tmpl:$f"
 done
+
+# Update GeoIP data
+/usr/bin/geoipupdate -v &
 
 # Setup cron job to update GeoIP data
 CRON_SCHEDULE="cron.weekly"
