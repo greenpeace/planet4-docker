1fa575f27a4c712721660ff7d461b59a1de14acb
Jen <jhasenau@greenpeace.org>
2020-01-24 11:50:22 +1100

    configure secrets for GeoIP and tag BAT tests to build-test in p4-fork

--- src/planet-4-151612/openresty/bin/nginx_configure_geoip2.sh
+++ src/planet-4-151612/openresty/bin/nginx_configure_geoip2.sh
@@ -2,30 +2,35 @@
 set -euo pipefail
 #Check License and Account key variables
 
+if [[ ${PHP_ENABLED} = 'true' ]]
+then
+  _warning "$(printf "%-10s " "openresty:")" "PHP is not compatible with GeoIP"
+  exit 0
+fi
+
 if [[ ${GEOIP2_ENABLED} = 'true' ]]
 then
   if [[ -z "${MAXMIND_ACCOUNTID}" ]]
   then
     GEOIP2_ENABLED="false"
     export GEOIP2_ENABLED
-    _warning "$(printf "%-10s " "php:")" "MAXMIND_ACCOUNTID is blank, account id is required"
-    _warning "$(printf "%-10s " "php:")" "disabling GeoIP"
+    _warning "$(printf "%-10s " "openresty:")" "MAXMIND_ACCOUNTID is blank, account id is required"
+    _warning "$(printf "%-10s " "openresty:")" "disabling GeoIP"
   elif [[ -z "${MAXMIND_LICENSE}" ]]
   then
     GEOIP2_ENABLED="false"
     export GEOIP2_ENABLED
-    _warning "$(printf "%-10s " "php:")" "MAXMIND_LICENSE is blank, license is required"
-    _warning "$(printf "%-10s " "php:")" "disabling GeoIP"
+    _warning "$(printf "%-10s " "openresty:")" "MAXMIND_LICENSE is blank, license is required"
+    _warning "$(printf "%-10s " "openresty:")" "disabling GeoIP"
   else
-    _good "$(printf "%-10s " "php:")" "$(printf "%-22s" "maxmind.accountid:")" "${MAXMIND_ACCOUNTID//[[:alnum:]]/*}"
-    _good "$(printf "%-10s " "php:")" "$(printf "%-22s" "maxmind.license:")" "${MAXMIND_LICENSE//[[:alnum:]]/*}"
+    _good "$(printf "%-10s " "openresty:")" "$(printf "%-22s" "maxmind.accountid:")" "${MAXMIND_ACCOUNTID//[[:alnum:]]/*}"
+    _good "$(printf "%-10s " "openresty:")" "$(printf "%-22s" "maxmind.license:")" "${MAXMIND_LICENSE//[[:alnum:]]/*}"
     _good "$(printf "%-10s " "openresty:")" "GEOIP2 ${GEOIP2_ENABLED}"
- fi
-fi
 
-[[ ${GEOIP2_ENABLED} = "true" ]] || {
-  exit 0
-}
+  fi
+
+if [[ ${GEOIP2_ENABLED} = 'true' ]]
+  then
 
 files=(
   /etc/nginx/conf.d/90_geoip.conf
@@ -48,3 +53,6 @@ GEOIP_CRON_FILE="/app/bin/nginx_update_geoip_database.sh"
 ln -s $GEOIP_CRON_FILE $GEOIP_WEEKLY_CRON_FILE_PATH
 
 wait
+
+  fi
+fi
--- tests/src/planet-4-151612/wordpress/tests/env.bash
+++ tests/src/planet-4-151612/wordpress/tests/env.bash
@@ -18,7 +18,7 @@ export image
 APP_HOSTNAME="www.planet4.test"
 DB_IMAGE="mysql:5.7"
 GIT_SOURCE="https://github.com/greenpeace/planet4-base-fork"
-GIT_BRANCH="develop"
+GIT_BRANCH="build-test"
 RANDOM_TITLE="Test-mcBkUCqAO3yCvAjy"
 WP_TITLE="Greenpeace - Testing"
 
