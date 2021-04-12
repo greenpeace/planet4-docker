1b549f4c91ce5c4d2eedd66748be361d15c253db
Nikos Roussos <nikos@roussos.cc>
2021-04-12 11:31:44 +0300

    PLANET-5963 Install APM agent and set configuration
    Ref: https://jira.greenpeace.org/browse/PLANET-5963
    
    ---
    
    Install APM php agent using their .deb package and doing some basic hash
    validation. Then use environment variables to populate its custom
    configuration file.
    
    For used `MYSQL_USER` for setting service name per app. It doesn't seem
    right, but it's the only variable I tested to give a meaningful value.
    Not sure why we duplicate config scripts on `my_init.d` and
    `rc.docker.d`, but I followed existing structure.

---
+++ src/planet-4-151612/wordpress/etc/my_init.d/30_install_elastic_apm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+wget --retry-connrefused --waitretry=1 -t 5 "https://github.com/elastic/apm-agent-php/releases/download/v1.0.1/apm-agent-php_${APM_AGENT_PHP_VERSION}_all.deb.sha512"
+wget --retry-connrefused --waitretry=1 -t 5 "https://github.com/elastic/apm-agent-php/releases/download/v1.0.1/apm-agent-php_${APM_AGENT_PHP_VERSION}_all.deb"
+
+sha512sum -c "apm-agent-php_${APM_AGENT_PHP_VERSION}_all.deb.sha512"
+
+dpkg -i "apm-agent-php_${APM_AGENT_PHP_VERSION}_all.deb"
