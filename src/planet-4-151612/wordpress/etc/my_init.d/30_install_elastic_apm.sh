ee0b7ff3c156bd2ec2f3184479e5ffee30fa9b5e
Florent Hernandez <florent.hernandez@greenpeace.org>
2021-06-14 16:31:43 +0200

    PLANET-5963: Add condition to APM agent installation
    Ref: https://jira.greenpeace.org/browse/PLANET-5963
    
    APM is not usually used on local environment, adding a variable
    to its installation process will allow to trigger or skip it,
    depending on the context.

--- src/planet-4-151612/wordpress/etc/my_init.d/30_install_elastic_apm.sh
+++ src/planet-4-151612/wordpress/etc/my_init.d/30_install_elastic_apm.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+[[ "${INSTALL_APM_AGENT}" = "true" ]] || exit 0
+
 wget --retry-connrefused --waitretry=1 -t 5 "https://github.com/elastic/apm-agent-php/releases/download/v1.0.1/apm-agent-php_${APM_AGENT_PHP_VERSION}_all.deb.sha512"
 wget --retry-connrefused --waitretry=1 -t 5 "https://github.com/elastic/apm-agent-php/releases/download/v1.0.1/apm-agent-php_${APM_AGENT_PHP_VERSION}_all.deb"
 
