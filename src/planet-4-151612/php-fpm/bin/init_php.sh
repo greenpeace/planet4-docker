0491e60a937bb70901e760cc84dfe8cd8830154c
Nikos Roussos <nikos@roussos.cc>
2021-09-06 14:09:06 +0300

    PLANET-6369 Phase out NewRelic
    Ref: https://jira.greenpeace.org/browse/PLANET-6369

--- src/planet-4-151612/php-fpm/bin/init_php.sh
+++ src/planet-4-151612/php-fpm/bin/init_php.sh
@@ -7,11 +7,6 @@ mkdir -p /run/php
 cp "/etc/php/${PHP_MAJOR_VERSION}/fpm/php.ini" "/etc/php/${PHP_MAJOR_VERSION}/fpm/php.ini.dist"
 cp "/etc/php/${PHP_MAJOR_VERSION}/fpm/pool.d/www.conf" "/etc/php/${PHP_MAJOR_VERSION}/fpm/pool.d/www.conf.dist"
 
-# Remove duplicate newrelic.ini files. Solves "Module 'newrelic' already loaded" warning message
-# See: https://discuss.newrelic.com/t/php-warning-module-newrelic-already-loaded-in-unknown-on-line-0/2903/21
-rm -f "/etc/php/${PHP_MAJOR_VERSION}/fpm/conf.d/newrelic.ini"
-rm -f "/etc/php/${PHP_MAJOR_VERSION}/cli/conf.d/newrelic.ini"
-
 # Don't fork
 sed -i -r "s/;daemonize = yes/daemonize = no/g" "/etc/php/${PHP_MAJOR_VERSION}/fpm/php-fpm.conf"
 
