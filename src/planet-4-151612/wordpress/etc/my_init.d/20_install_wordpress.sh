ab9cd4f8cc86182abecb785b2d4d2c88df9740c3
inwerpsel <pieter.vincent1988@gmail.com>
2021-09-21 12:48:00 +0200

    Read WP_VERSION only from Composer files
    * Remove read from env var `WP_VERSION`.
    * Exit before exporting if var is empty.

--- src/planet-4-151612/wordpress/etc/my_init.d/20_install_wordpress.sh
+++ src/planet-4-151612/wordpress/etc/my_init.d/20_install_wordpress.sh
@@ -242,16 +242,18 @@ fi
 
 chown -R "${APP_USER}:${APP_USER}" "$PUBLIC_PATH"
 
+WP_VERSION=$(jq -r '.extra["wp-version"] // empty' <"${SOURCE_PATH}"/composer-local.json)
 if [[ -z "${WP_VERSION}" ]]; then
   WP_VERSION=$(jq -r '.extra["wp-version"] // empty' <"${SOURCE_PATH}"/composer.json)
 fi
-export WP_VERSION
-echo "Using WP_VERSION: ${WP_VERSION}"
 
 if [ -z "$WP_VERSION" ]; then
   echo "WP_VERSION not set"
   exit 1
 fi
+
+export WP_VERSION
+echo "Using WP_VERSION: ${WP_VERSION}"
 
 wp --root core download --version="${WP_VERSION}" --force "${WP_DOWNLOAD_FLAGS}"
 
