01f3736334fe89a435b244f53e73645bd83f31d0
Nikos Roussos <nikos.roussos@greenpeace.org>
2025-01-17 17:05:21 +0200

    Clean up unused Elastic APM config

--- src/planet-4-151612/wordpress/etc/my_init.d/95_elastic_apm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if [[ -d "/opt/elastic/apm-agent-php/etc/" ]]; then
-  dockerize -template /app/templates/etc/php/fpm/conf.d/30-elastic-apm-custom.ini.tmpl:/opt/elastic/apm-agent-php/etc/elastic-apm-custom.ini
-fi
